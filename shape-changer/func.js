5840ba0c41bd7f363ae4dd0632123eaa9bc10b09
Daoist-Insanity <roy.devrup@gmail.com>
2020-12-21 19:51:44 +0530

    first commit shapes

---
+++ shape-changer/func.js
@@ -0,0 +1,23 @@
+function rectangle(){ 
+    document.getElementById('shape').style['margin-left']='90px'; 
+    document.getElementById('shape').style['height']='150px';
+    document.getElementById('shape').style['width']='300px';
+    document.getElementById('shape').style['background-color']='red';
+    document.getElementById('shape').style['border-radius']='0%';
+    
+  }
+  function sqaure(){  
+    document.getElementById('shape').style['margin-left']='170px'; 
+    document.getElementById('shape').style['height']='150px';
+    document.getElementById('shape').style['width']='150px';
+    document.getElementById('shape').style['background-color']='green';
+    document.getElementById('shape').style['border-radius']='0%';
+    }
+
+    function circle(){ 
+    document.getElementById('shape').style['margin-left']='170px'; 
+    document.getElementById('shape').style['height']='150px';
+    document.getElementById('shape').style['width']='150px';
+    document.getElementById('shape').style['background-color']='yellow';
+    document.getElementById('shape').style['border-radius']='100%';
+    }
